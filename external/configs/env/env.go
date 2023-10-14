c3eb5821664ff38e08deb77a7ced7fb35cd3f5f4
IsaacDSC <isaac8.silva@hotmail.com>
2023-10-14 19:34:45 -0300

    feat: add configurations environments

---
+++ external/configs/env/env.go
@@ -0,0 +1,41 @@
+package env
+
+import (
+	"log"
+
+	"github.com/joho/godotenv"
+)
+
+type Environment struct {
+	DATABASE_URL string
+	GODEV        string
+	PORT         string
+	REDIS_URL    string
+}
+
+var datasetEnv *Environment
+
+func GetEnv() *Environment {
+	return datasetEnv
+}
+
+func StartEnv(path string) *Environment {
+	envs, err := godotenv.Read(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var (
+		port string = "3000"
+	)
+	if len(envs["PORT"]) > 0 {
+		port = envs["PORT"]
+	}
+	// godev, _ := strconv.ParseBool(envs["GODEV"])
+	datasetEnv = &Environment{
+		DATABASE_URL: envs["DATABASE_URL"],
+		REDIS_URL:    envs["REDIS_URL"],
+		GODEV:        envs["GODEV"],
+		PORT:         port,
+	}
+	return datasetEnv
+}
