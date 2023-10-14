bc662c7c1aa28675db91575d0e99b74173fa9b37
IsaacDSC <isaac8.silva@hotmail.com>
2023-10-14 18:21:49 -0300

    feat: seed populate users in database

---
+++ external/sqlc/seed/user.seed.sql
@@ -0,0 +1,30 @@
+INSERT INTO
+  "user" (
+    complete_name,
+    cpf_cnpj,
+    type_user,
+    email,
+    password
+  )
+VALUES
+  (
+    'Advino Silva Name',
+    '02408813000140',
+    'STOREKEEPER',
+    'advino@gmail.com',
+    '$2y$10$JO5.D5jFWIzmfEbkBfA27u/ozwvFA/1/MkS7yfjpmcSRvEf8GAEu.'
+  ),
+  (
+    'Fulano Silva Name',
+    '21619208000163',
+    'STOREKEEPER',
+    'fulano@gmail.com',
+    '$2y$10$JO5.D5jFWIzmfEbkBfA27u/ozwvFA/1/MkS7yfjpmcSRvEf8GAEu.'
+  ),
+(
+    'Jucelino Silva Pereira Name',
+    '59528579060',
+    'CONSUMER',
+    'jucelino@gmail.com',
+    '$2y$10$zaiMbeXWtM0TmufYpnQjBOVmYSgSZjxqlZRVOrgct5WOnvCHwM3iO'
+  );
