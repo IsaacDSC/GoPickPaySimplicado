4ab957724525ce95f5882d17759b3c700537137e
IsaacDSC <isaac8.silva@hotmail.com>
2023-10-17 09:52:27 -0300

    feat: minimal transaction service

--- internal/service/transaction.service.go
+++ internal/service/transaction.service.go
@@ -1,8 +1,12 @@
 package service
 
 import (
+	"context"
+	"fmt"
+
 	"github.com/IsaacDSC/GoPickPaySimplicado/internal/domain"
 	"github.com/IsaacDSC/GoPickPaySimplicado/internal/infra/contracts"
+	"github.com/IsaacDSC/GoPickPaySimplicado/internal/shared/dto"
 )
 
 type TransactionServiceInterface interface {
@@ -10,23 +14,122 @@ type TransactionServiceInterface interface {
 }
 
 type TransactionService struct {
-	userRepo        contracts.UserRepositoryInterface
-	transactionRepo contracts.TransactionRepositoriesInterface
-	checkOperation  contracts.OperationTransactionGatewayInterface
-	notification    contracts.NotificationMailerInterface
-	transaction     domain.TransactionEntity //TODO: implementar contratos
-	wallet          domain.WalletEntity      //TODO: implementar contratos
+	userRepo         contracts.UserRepositoryInterface
+	transactionRepo  contracts.TransactionRepositoriesInterface
+	checkOperation   contracts.OperationTransactionGatewayInterface
+	notification     contracts.NotificationMailerInterface
+	transactionPayer *domain.TransactionEntity //TODO: implementar contratos
+	transactionPayee *domain.TransactionEntity //TODO: implementar contratos
+	input            dto.TransactionDtoInput
 }
 
-func (*TransactionService) Transfer() (list_errors []error) {
+func NewTransactionService(
+	userRepo contracts.UserRepositoryInterface,
+	transactionRepo contracts.TransactionRepositoriesInterface,
+	checkOperation contracts.OperationTransactionGatewayInterface,
+	notification contracts.NotificationMailerInterface,
+	transactionPayer *domain.TransactionEntity,
+	transactionPayee *domain.TransactionEntity,
+) *TransactionService {
+	return &TransactionService{
+		userRepo,
+		transactionRepo,
+		checkOperation,
+		notification,
+		transactionPayer,
+		transactionPayee,
+		dto.TransactionDtoInput{},
+	}
+}
+
+func (ts *TransactionService) Transfer(ctx context.Context, input dto.TransactionDtoInput) (list_errors []error) {
+	ts.input = input
+	list_errors = ts.executeTransactionPayee(ctx)
+	if len(list_errors) != 0 {
+		fmt.Println("primeiro error")
+		return
+	}
+	list_errors = ts.executeTransactionPayer(ctx)
+	if len(list_errors) != 0 {
+		fmt.Println("segundo error")
+		return
+	}
 	//RECEBER DADOS DTO (PAYEE PAYER VALUE)
-	//BUSCAR PAYER
-	//BUSCAR PAYEE
-	// VALIDAR TRANSACTION PAYER
-	// VALIDAR TRANSACTION PAYEE
 	//CHECK-OPERATION
 	//SAVE TRANSACTION PAYER
 	// SAVE TRANSACTION PAYEE
 	//NOTIFY EMAIL
+	return
+}
+
+func (ts *TransactionService) executeTransactionPayer(ctx context.Context) (list_errors []error) {
+	payer, err := ts.userRepo.GetUserByID(ctx, ts.input.PayerID)
+	if err != nil {
+		list_errors = append(list_errors, err)
+		return
+	}
+	transactionsPayer, err := ts.transactionRepo.GetTransactionsByUserID(ctx, payer.ID)
+	if err != nil {
+		list_errors = append(list_errors, err)
+		return
+	}
+	var transactions []domain.Transactions
+	for index := range transactionsPayer {
+		transactions = append(transactions, domain.Transactions{
+			ID:        transactionsPayer[index].ID,
+			TypeUser:  transactionsPayer[index].TypeUser,
+			Status:    transactionsPayer[index].Status,
+			Value:     transactionsPayer[index].Value,
+			Operation: transactionsPayer[index].Operation,
+		})
+	}
+	ts.transactionPayee.TransactionFactory(
+		payer.TypeUser, ts.input.Value, true, transactions,
+	)
+	list_errors = ts.transactionPayee.Transaction()
+	if len(list_errors) != 0 {
+		return
+	}
+	err = ts.transactionRepo.InsertTransaction(ctx, *ts.transactionPayee)
+	if err != nil {
+		list_errors = append(list_errors, err)
+		return
+	}
+	return
+}
+
+func (ts *TransactionService) executeTransactionPayee(ctx context.Context) (list_errors []error) {
+	payee, err := ts.userRepo.GetUserByID(ctx, ts.input.PayeeID)
+	if err != nil {
+		list_errors = append(list_errors, err)
+		return
+	}
+	transactionsPayee, err := ts.transactionRepo.GetTransactionsByUserID(ctx, payee.ID)
+	if err != nil {
+		list_errors = append(list_errors, err)
+		return
+	}
+	var transactions []domain.Transactions
+	for index := range transactionsPayee {
+		transactions = append(transactions, domain.Transactions{
+			ID:        transactionsPayee[index].ID,
+			TypeUser:  transactionsPayee[index].TypeUser,
+			Status:    transactionsPayee[index].Status,
+			Value:     transactionsPayee[index].Value,
+			Operation: transactionsPayee[index].Operation,
+		})
+	}
+	ts.transactionPayee.TransactionFactory(
+		payee.TypeUser, ts.input.Value, false, transactions,
+	)
+	list_errors = ts.transactionPayee.Transaction()
+	if len(list_errors) != 0 {
+		return
+	}
+	err = ts.transactionRepo.InsertTransaction(ctx, *ts.transactionPayer)
+	if err != nil {
+		list_errors = append(list_errors, err)
+		return
+	}
 	return
 }
---
+++ internal/service/transaction.service_test.go
@@ -0,0 +1,125 @@
+package service
+
+import (
+	"context"
+	"testing"
+
+	"github.com/IsaacDSC/GoPickPaySimplicado/external/mocks"
+	"github.com/IsaacDSC/GoPickPaySimplicado/internal/domain"
+	"github.com/IsaacDSC/GoPickPaySimplicado/internal/shared/dto"
+	"github.com/golang/mock/gomock"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTransactionService(t *testing.T) {
+	t.Run("Given init transaction payer", func(t *testing.T) {
+		t.Run("When send input valid", func(t *testing.T) {
+			t.Run("Then transaction not return error", func(t *testing.T) {
+				ctl := gomock.NewController(t)
+				defer ctl.Finish()
+				userRepository := mocks.NewMockUserRepositoryInterface(ctl)
+				userID := uuid.New()
+				userRepository.EXPECT().GetUserByID(gomock.Any(), gomock.Any()).Return(
+					domain.UserEntity{
+						ID:           userID,
+						CompleteName: "Advino Silva Name",
+						Cpf_cnpj:     "1233213123123123",
+						TypeUser:     "CONSUMER",
+						Email:        "advino@gamil.com",
+					}, nil,
+				)
+				transactionRepository := mocks.NewMockTransactionRepositoriesInterface(ctl)
+				transactionRepository.EXPECT().GetTransactionsByUserID(gomock.Any(), gomock.Any()).Return(
+					[]domain.Transactions{
+						{
+							ID:        uuid.New(),
+							TypeUser:  "CONSUMER",
+							Status:    "COMPLETE",
+							Value:     "50000000",
+							Operation: "CREDIT",
+						},
+						{
+							ID:        uuid.New(),
+							TypeUser:  "CONSUMER",
+							Status:    "COMPLETE",
+							Value:     "50000000",
+							Operation: "CREDIT",
+						},
+					}, nil,
+				)
+				transactionRepository.EXPECT().InsertTransaction(gomock.Any(), gomock.Any()).Return(nil)
+				service := NewTransactionService(
+					userRepository, transactionRepository,
+					mocks.NewMockOperationTransactionGatewayInterface,
+					mocks.NewMockNotificationMailerInterface,
+					domain.NewTransactionEntity(domain.NewWalletEntity()),
+					domain.NewTransactionEntity(domain.NewWalletEntity()),
+				)
+				service.input = dto.TransactionDtoInput{
+					Value:   "10000000",
+					PayerID: userID,
+				}
+				list_errors := service.executeTransactionPayer(context.Background())
+				for index := range list_errors {
+					assert.NoError(t, list_errors[index])
+				}
+			})
+		})
+	})
+
+	t.Run("Given init transaction payer", func(t *testing.T) {
+		t.Run("When send input valid", func(t *testing.T) {
+			t.Run("Then transaction not return error", func(t *testing.T) {
+				ctl := gomock.NewController(t)
+				defer ctl.Finish()
+				userRepository := mocks.NewMockUserRepositoryInterface(ctl)
+				userID := uuid.New()
+				userRepository.EXPECT().GetUserByID(gomock.Any(), gomock.Any()).Return(
+					domain.UserEntity{
+						ID:           userID,
+						CompleteName: "Advino Silva Name",
+						Cpf_cnpj:     "1233213123123123",
+						TypeUser:     "STOREKEEPER",
+						Email:        "advino@gamil.com",
+					}, nil,
+				)
+				transactionRepository := mocks.NewMockTransactionRepositoriesInterface(ctl)
+				transactionRepository.EXPECT().GetTransactionsByUserID(gomock.Any(), gomock.Any()).Return(
+					[]domain.Transactions{
+						{
+							ID:        uuid.New(),
+							TypeUser:  "CONSUMER",
+							Status:    "COMPLETE",
+							Value:     "50000000",
+							Operation: "CREDIT",
+						},
+						{
+							ID:        uuid.New(),
+							TypeUser:  "CONSUMER",
+							Status:    "COMPLETE",
+							Value:     "50000000",
+							Operation: "CREDIT",
+						},
+					}, nil,
+				)
+				transactionRepository.EXPECT().InsertTransaction(gomock.Any(), gomock.Any()).Return(nil)
+				service := NewTransactionService(
+					userRepository, transactionRepository,
+					mocks.NewMockOperationTransactionGatewayInterface,
+					mocks.NewMockNotificationMailerInterface,
+					domain.NewTransactionEntity(domain.NewWalletEntity()),
+					domain.NewTransactionEntity(domain.NewWalletEntity()),
+				)
+				service.input = dto.TransactionDtoInput{
+					Value:   "10000000",
+					PayerID: userID,
+				}
+				list_errors := service.executeTransactionPayee(context.Background())
+				for index := range list_errors {
+					assert.NoError(t, list_errors[index])
+				}
+			})
+		})
+	})
+}
