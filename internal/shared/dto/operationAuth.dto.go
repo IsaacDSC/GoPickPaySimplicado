aed45036aa97001b76a2632d62a31e9aa11e92e8
IsaacDSC <isaac8.silva@hotmail.com>
2023-10-22 16:00:37 -0300

    feat: dto transaction and operation auth transaction

---
+++ internal/shared/dto/operationAuth.dto.go
@@ -0,0 +1,5 @@
+package dto
+
+type OperationAuthResponse struct {
+	Status string `json:"status"`
+}
--- internal/shared/dto/transaction.dto.go
+++ internal/shared/dto/transaction.dto.go
@@ -4,6 +4,7 @@ import "github.com/google/uuid"
 
 type TransactionDtoInput struct {
 	Value   string    `json:"value"`
-	PayerID uuid.UUID `json:"payer"`
-	PayeeID uuid.UUID `json:"payee"`
+	PayerID uuid.UUID `json:"payerId"`
+	PayeeID uuid.UUID `json:"payeeId"`
+	Sleep   int       `json:"sleep"`
 }
