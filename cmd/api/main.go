80f937b6edf97090f03f9991ed7f76115e3b1209
IsaacDSC <isaac8.silva@hotmail.com>
2023-10-28 09:44:27 -0300

    fix: remove queue producer starter main

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -7,7 +7,6 @@ import (
 	"github.com/IsaacDSC/GoPickPaySimplicado/external/configs/queue"
 	web "github.com/IsaacDSC/GoPickPaySimplicado/web/http"
 	"github.com/IsaacDSC/GoPickPaySimplicado/web/tasks"
-	"github.com/google/uuid"
 )
 
 func init() {
@@ -18,9 +17,5 @@ func init() {
 func main() {
 	go queue.Consumer()
 	go tasks.StartServerTasks()
-	producer := queue.NewProducerQueue(queue.AsyncClientConn())
-	producer.TransactionNotificationMailer(
-		uuid.New(), "isaacdsc@gmail.com",
-	)
 	web.StartServerHttp()
 }
