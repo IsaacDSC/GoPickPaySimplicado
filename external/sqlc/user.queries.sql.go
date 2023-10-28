c65f5abc353ef1b1e467a95123f8ca90c6e91cfd
IsaacDSC <isaac8.silva@hotmail.com>
2023-10-28 09:48:46 -0300

    feat: transaction queries

--- external/sqlc/user.queries.sql.go
+++ external/sqlc/user.queries.sql.go
@@ -7,6 +7,7 @@ package sqlc
 
 import (
 	"context"
+	"database/sql"
 
 	"github.com/google/uuid"
 )
@@ -65,4 +66,65 @@ func (q *Queries) GetUserByID(ctx context.Context, id uuid.UUID) (User, error) {
 		&i.CreatedAt,
 	)
 	return i, err
+}
+
+const transactionsOnUser = `-- name: TransactionsOnUser :many
+select "user".id, complete_name, cpf_cnpj, type_user, email, password, "user".updated_at, "user".created_at, transaction.id, user_id, value, operation, status, transaction.updated_at, transaction.created_at from "user" join "transaction" on "transaction".user_id = "user".id
+`
+
+type TransactionsOnUserRow struct {
+	ID           uuid.UUID
+	CompleteName sql.NullString
+	CpfCnpj      string
+	TypeUser     string
+	Email        string
+	Password     sql.NullString
+	UpdatedAt    sql.NullTime
+	CreatedAt    sql.NullTime
+	ID_2         uuid.UUID
+	UserID       uuid.UUID
+	Value        string
+	Operation    sql.NullString
+	Status       string
+	UpdatedAt_2  sql.NullTime
+	CreatedAt_2  sql.NullTime
+}
+
+func (q *Queries) TransactionsOnUser(ctx context.Context) ([]TransactionsOnUserRow, error) {
+	rows, err := q.db.QueryContext(ctx, transactionsOnUser)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var items []TransactionsOnUserRow
+	for rows.Next() {
+		var i TransactionsOnUserRow
+		if err := rows.Scan(
+			&i.ID,
+			&i.CompleteName,
+			&i.CpfCnpj,
+			&i.TypeUser,
+			&i.Email,
+			&i.Password,
+			&i.UpdatedAt,
+			&i.CreatedAt,
+			&i.ID_2,
+			&i.UserID,
+			&i.Value,
+			&i.Operation,
+			&i.Status,
+			&i.UpdatedAt_2,
+			&i.CreatedAt_2,
+		); err != nil {
+			return nil, err
+		}
+		items = append(items, i)
+	}
+	if err := rows.Close(); err != nil {
+		return nil, err
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return items, nil
 }
